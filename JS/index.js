800456680d6d40825dce29c3776f2ab0d7095222
刘坤 <593062785@qq.com>
2021-09-30 14:07:51 +0800

    初始化

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+Vue.config.productionTip=false
+        new Vue({
+            el:'#root',
+            data:{
+                name:"world1"
+            },
+            computed: {
+                
+            },
+            methods: {
+                
+            },
+        })
